a773fd749d32b7ba0afd23fe38d02dbbcf4c2eaa
kim-daeyong <kdy2353@gmail.com>
2019-06-04 18:39:18 +0900

    index(view), mainController 작성

--- src/main/java/example/daeyong/kim/exratecalculator/controller/MainController.java
+++ src/main/java/example/daeyong/kim/exratecalculator/controller/MainController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
 
-    @GetMapping("/main")
+    @GetMapping("/")
     private String main() {
         return "index";
     }
