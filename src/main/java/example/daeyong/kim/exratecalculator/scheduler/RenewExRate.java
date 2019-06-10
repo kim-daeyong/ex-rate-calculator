89b973f3036ded146683ee44dd5b651c07877d87
kim-daeyong <kdy2353@gmail.com>
2019-06-10 17:45:32 +0900

    ReadMe 수정

--- src/main/java/example/daeyong/kim/exratecalculator/scheduler/RenewExRate.java
+++ src/main/java/example/daeyong/kim/exratecalculator/scheduler/RenewExRate.java
@@ -16,7 +16,7 @@ public class RenewExRate {
     @Autowired
     ExRateAPIService exRateAPIService;
 
-    @Scheduled(cron = "0 0 */1 * * *")
+    @Scheduled(cron = "0 0 0 * * *")
     public String renweExRate(){
         exRateAPIService.renewAPI();
 
